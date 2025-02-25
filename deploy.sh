b69327eb187d101117a3aaeb2771e7a00c9411e5
Endrick <EPolFlo203@outlook.com>
2025-02-24 20:05:23 -0600

    Corrección de errores

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
-#!/ Apagar NGINX y NGROK
+#!/bin/bash
+# Apagar NGINX y NGROK
 sudo systemctl stop NGINX
 pkill ngrok
 
-#!/ Clonar repositorio
-git pull origin main
+# Clonar repositorio
+sudo git pull origin main
 
-#!/ Encender NGINX
+# Encender NGINX
 sudo systemctl start NGINX
 
-#!/ Generar URL de NGROK
+# Generar URL de NGROK
 ngrok http 80 > /dev/null &
 
-#!/ Desplegar la URL de NGROK
+# Desplegar la URL de NGROK
 sleep 3
 URL=$(curl -s http://localhost:4040/api/tunnels | grep -o 'https://[^"]*')
 echo "Url= $URL"
